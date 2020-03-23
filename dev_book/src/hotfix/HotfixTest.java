5938451dd4ca77478bdea7c28bea14935367edd9
slalom0914 <slalom0914@gmail.com>
2020-03-23 15:43:43 +0900

    hotfix브랜치로 파일추가함. 3시43분

---
+++ dev_book/src/hotfix/HotfixTest.java
@@ -0,0 +1,10 @@
+package hotfix;
+
+public class HotfixTest {
+
+	public static void main(String[] args) {
+	//프로젝트명->오른쪽->Remote->Switch-> New Branch -> hotfix	
+		System.out.println("hotfix 브랜치에서 추가한 소스임.");
+	}
+
+}
