5492b2e603ee7b6fe334b010cfb00930a81cc36d
slalom0914 <slalom0914@gmail.com>
2020-03-23 16:57:58 +0900

    hotfix브랜치에서 저장점을 네개더 가지기 위한 소스 추가

---
+++ dev_book/src/hotfix/HotfixTest4.java
@@ -0,0 +1,10 @@
+package hotfix;
+
+public class HotfixTest4 {
+
+	public static void main(String[] args) {
+	//hotfix브랜치에서 저장점을 네개더 가지기 위한 소스 추가
+		System.out.println("hotfix 브랜치에서 네번째로 추가한 소스임.");
+	}
+
+}
