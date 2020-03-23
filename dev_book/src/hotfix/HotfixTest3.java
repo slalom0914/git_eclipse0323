3cce7fd26588a6a4f33b9a8e36e52b1ac8bfe9ff
slalom0914 <slalom0914@gmail.com>
2020-03-23 16:42:29 +0900

    hotfix브랜치에서 저장점을 세개더 가지기 위한 소스 추가

---
+++ dev_book/src/hotfix/HotfixTest3.java
@@ -0,0 +1,10 @@
+package hotfix;
+
+public class HotfixTest3 {
+
+	public static void main(String[] args) {
+	//hotfix브랜치에서 저장점을 세개더 가지기 위한 소스 추가
+		System.out.println("hotfix 브랜치에서 3번째로 추가한 소스임.");
+	}
+
+}
